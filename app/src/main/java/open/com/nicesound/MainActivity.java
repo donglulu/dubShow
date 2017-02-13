779dbce02b60967134a8c2e186ee1d37f62a3272
WIN7-20161121ZY\Administrator <gino1993>
2017-02-13 15:13:00 +0800

    1
    124

--- app/src/main/java/open/com/nicesound/MainActivity.java
+++ app/src/main/java/open/com/nicesound/MainActivity.java
@@ -2,12 +2,21 @@ package open.com.nicesound;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.activity_main).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
