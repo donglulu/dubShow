311ca1d406879c30d0988cf51257dac67a979d16
1319470638 <1319470638@qq.com>
2017-02-23 10:14:27 +0800

    简单的写了些圈子部分的界面

---
+++ app/src/main/java/open/com/nicesound/view/circle/Circle.java
@@ -0,0 +1,53 @@
+package open.com.nicesound.view.circle;
+
+
+
+/**
+ * Created by fight on 2017/2/16.
+ */
+
+public class Circle {
+    private String name;
+    private String img;
+    private String postNum;
+    private String attentionNum;
+
+    public Circle(String name, String img, String postNum, String attentionNum) {
+        this.name = name;
+        this.img = img;
+        this.postNum = postNum;
+        this.attentionNum = attentionNum;
+    }
+
+    public String getPostNum() {
+        return postNum;
+    }
+
+    public void setPostNum(String postNum) {
+        this.postNum = postNum;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getAttentionNum() {
+        return attentionNum;
+    }
+
+    public void setAttentionNum(String attentionNum) {
+        this.attentionNum = attentionNum;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/open/com/nicesound/view/circle/CircleFragment.java
+++ app/src/main/java/open/com/nicesound/view/circle/CircleFragment.java
@@ -1,10 +1,20 @@
 package open.com.nicesound.view.circle;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import open.com.nicesound.R;
 import open.com.nicesound.base.BaseFragment;
@@ -17,14 +27,53 @@ import open.com.nicesound.base.BaseFragment;
 
 public class CircleFragment extends BaseFragment {
 
-  @Override
+
+    private Map<String,String> map;
+    private List<Map<String,String>> listData;
+    private CircleAdapter adapter;
+    private View view;
+    ListView listView;
+
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.circle_fragment, container, false);
+        view=inflater.inflate(R.layout.circle_fragment, container, false);
+        initView();
+
+        return view;
     }
+
+
+    public void initView(){
+        TextView textView=(TextView) view.findViewById(R.id.title_tv);
+        listView =(ListView) view.findViewById(R.id.lv);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(getActivity(),CircleDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+        initDate();
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getActivity(),CircleDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+
     }
     public static CircleFragment newInstance() {
 
@@ -33,5 +82,18 @@ public class CircleFragment extends BaseFragment {
         return fragment;
     }
 
+    public void initDate(){
+        listData=new ArrayList<Map<String,String>>();
+        for (int i = 0; i < 5; i++) {
+            map = new HashMap<String, String>();
+            map.put("name", "name" + i);
+            map.put("img", "name" + i);
+            map.put("postNum", "postNum" + i);
+            map.put("attentionNum", "postNum" + i);
+            listData.add(map);
+        }
+        adapter =new CircleAdapter(listData,getActivity(),R.layout.circle_lv_item);
+        listView.setAdapter(adapter);
+    }
 
 }
